3aa593c99e8081d7a5ca8b8040e9fb566fc0e620
namankrishnabirthray <37155056+namankrishnabirthray@users.noreply.github.com>
2020-05-19 22:53:23 +0530

    Add files via upload

---
+++ build/precache-manifest.0c8c47e436c56b74ae47cd031f0130a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0517279fd5508177003e300ebf6d272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a110ac0eb1e41ccd23",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "daae15ca7d60cc55adbf",
+    "url": "/static/js/2.2472272c.chunk.js"
+  },
+  {
+    "revision": "b6a110ac0eb1e41ccd23",
+    "url": "/static/js/main.272f05dd.chunk.js"
+  },
+  {
+    "revision": "d006129aad6889348f63",
+    "url": "/static/js/runtime-main.c0b3e2f5.js"
+  }
+]);
